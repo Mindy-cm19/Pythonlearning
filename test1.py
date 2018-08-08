26192a2a68d476ddf1c6190b09fc8dc47e103476
Mindy-cm19 <37869598+Mindy-cm19@users.noreply.github.com>
2018-08-08 19:46:41 +0800

    Create test1.py
    函数-函数的参数

---
+++ test1.py
@@ -0,0 +1,12 @@
+"""
+写一个 可接收一个或多个数并计算乘积 的函数
+"""
+
+def product(*args):
+    if len(args)==0:
+        raise TypeError('不能没有参数输入!')
+    else:
+        s=1
+        for i in args:
+            s=s*i
+        return s
